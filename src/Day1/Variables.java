45a2a16750f455ea2752dad104d1dfb555b7854a
MeghanaGolla <meghanagolla33@gmail.com>
2025-07-14 12:46:28 +0530

    Updates Variables

--- src/Day1/Variables.java
+++ src/Day1/Variables.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Variables {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name:");
         String name = sc.nextLine();
+        System.out.println("Enter age");
         int age = sc.nextInt();
+        System.out.println("Enter cgpa");
         float cgpa = sc.nextFloat();
         System.out.println("Name: " +name);
         System.out.println("Age: " +age);
